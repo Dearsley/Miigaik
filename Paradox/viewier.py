e1b4c0afd2118a9fb3cc6c219edc1cdcdca1c2db
dearsley <ganohin431@gmail.com>
2023-11-09 16:52:50 +0300

    Paradox minor fixes

--- Paradox/viewier.py
+++ Paradox/viewier.py
@@ -3,20 +3,25 @@ from Paradox.birthday import *
 
 
 def show():
-    monty_hall_iteration = input("Введите количество итераций: ")
-    birthday_group = input("Введите количество человек в группе: ")
-    birthday_iteration = input("Введите количество итераций: ")
+    print("Выберите парадокс: \n1. Monty Hall \n2. Birthday")
+    chosen = input()
+    match chosen:
+        case "1":
+            monty_hall_iteration = input("Введите количество итераций: ")
 
-    print(f"Monty Hall: {monty_hall(int(monty_hall_iteration))} %" if monty_hall_iteration.isdigit() else f"Monty Hall: {monty_hall()} %")
+            print(f"Monty Hall: {monty_hall(int(monty_hall_iteration))} %" if monty_hall_iteration.isdigit() else f"Monty Hall: {monty_hall()} %")
+        case "2":
+            birthday_group = input("Введите количество человек в группе: ")
+            birthday_iteration = input("Введите количество итераций: ")
 
-    if birthday_iteration.isdigit():
-        print(f"Birthday: {birthday(iterations=int(birthday_iteration))} %")
-    elif birthday_group.isdigit():
-        print(f"Birthday: {birthday(people_in_group=int(birthday_group))} %")
-    elif birthday_iteration.isdigit() and birthday_group.isdigit():
-        print(f"Birthday: {birthday(int(birthday_group), int(birthday_iteration))} %")
-    else:
-        print(f"Birthday: {birthday()} %")
+            if birthday_iteration.isdigit():
+                print(f"Birthday: {birthday(iterations=int(birthday_iteration))} %")
+            elif birthday_group.isdigit():
+                print(f"Birthday: {birthday(people_in_group=int(birthday_group))} %")
+            elif birthday_iteration.isdigit() and birthday_group.isdigit():
+                print(f"Birthday: {birthday(int(birthday_group), int(birthday_iteration))} %")
+            else:
+                print(f"Birthday: {birthday()} %")
 
     if __name__ == "__main__":
         show()
